4eee9585ae2e90e0046ac4f9233d65bac99bcf0e
Dmitry Sergeev <lbvfcthuttd1234@gmail.com>
2025-02-09 21:07:12 +0400

    header-ready

---
+++ src/components/logo-div/index.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import { Logo } from '../../icons/Logo'
+import { Title } from '../main/Title'
+
+const LogoDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 50px;
+`
+
+export function LogoComponent() {
+  return (
+    <LogoDiv>
+      <Logo />
+      <Title>WILDLIFE</Title>
+    </LogoDiv>
+  )
+}
---
+++ src/components/main/Navi/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Navi = styled.li`
+  a {
+    margin-right: 60px;
+    font-family: 'Open Sans';
+    font-size: 23px;
+    font-weight: 400;
+    line-height: 60px;
+    text-align: left;
+    text-underline-position: from-font;
+    text-decoration-skip-ink: none;
+    color: #f7f7f7;
+  }
+`
